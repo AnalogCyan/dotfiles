86695fccf83142a56731303b0cd28b01469f9354
Cyan Thayn <git@thayn.me>
2019-10-15 14:40:00 -0500

    Removed -i flag in cp actions.

--- Linux/install.sh
+++ Linux/install.sh
@@ -16,23 +16,23 @@ read -r gitconf
 if [ "$ffunc" = "y" ] || [ "$ffunc" = "Y" ]; then
   echo "Copying fish functions into ~/.config/fish/functions/..."
   mkdir -pv ~/.config/fish/functions/
-  cp -i ./functions/*.fish ~/.config/fish/functions/
+  cp ./functions/*.fish ~/.config/fish/functions/
 fi
 
 if [ "$lxconf" = "y" ] || [ "$lxconf" = "Y" ]; then
   echo "Copying lxterm config into ~/.config/lxterminal/lxterminal.conf..."
   mkdir -pv ~/.config/lxterminal/
-  cp -i ./lxterminal.conf ~/.config/lxterminal/lxterminal.conf
+  cp ./lxterminal.conf ~/.config/lxterminal/lxterminal.conf
 fi
 
 if [ "$cconf" = "y" ] || [ "$cconf" = "Y" ]; then
   echo "Copying conky configs into ~/.config/conky..."
   mkdir -pv ~/.config/conky/
-  cp -i ./conky/rc.glsl ~/.config/conky/rc.glsl
-  cp -i ./conky/Arco-logos.conkyrc ~/.config/conky/Arco-logos.conkyrc
+  cp ./conky/rc.glsl ~/.config/conky/rc.glsl
+  cp ./conky/Arco-logos.conkyrc ~/.config/conky/Arco-logos.conkyrc
 fi
 
 if [ "$gitconf" = "y" ] || [ "$gitconf" = "Y" ]; then
   echo "Copying git config into ~/.gitconfig..."
-  cp -i ./.gitconfig ~/.gitconfig
+  cp ./.gitconfig ~/.gitconfig
 fi
