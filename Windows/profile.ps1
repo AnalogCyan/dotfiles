c435c841c1d04c23ba5334aa141788dff2e5d99a
Cyan Thayn <git@thayn.me>
2020-09-23 19:39:31 -0500

    Added winget autocomplete.

--- Windows/profile.ps1
+++ Windows/profile.ps1
@@ -39,7 +39,18 @@ Function Prompt {
   # $($executionContext.SessionState.Path.CurrentLocation)
 }
 
-# Chocolatey profile
+# Windows Package Manager
+Register-ArgumentCompleter -Native -CommandName winget -ScriptBlock {
+  param($wordToComplete, $commandAst, $cursorPosition)
+  [Console]::InputEncoding = [Console]::OutputEncoding = $OutputEncoding = [System.Text.Utf8Encoding]::new()
+  $Local:word = $wordToComplete.Replace('"', '""')
+  $Local:ast = $commandAst.ToString().Replace('"', '""')
+  winget complete --word="$Local:word" --commandline "$Local:ast" --position $cursorPosition | ForEach-Object {
+    [System.Management.Automation.CompletionResult]::new($_, $_, 'ParameterValue', $_)
+  }
+}
+
+# Chocolatey
 $ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
 if (Test-Path($ChocolateyProfile)) {
   Import-Module "$ChocolateyProfile"
