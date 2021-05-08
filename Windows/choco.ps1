02530bccb478ff0fd771fcffd2038d7059f49e72
AnalogCyan <git@thayn.me>
2021-05-08 14:06:44 -0500

    Add winget/choco package lists.

---
+++ Windows/choco.ps1
@@ -0,0 +1,24 @@
+choco upgrade autohotkey -y
+choco upgrade choco-protocol-support -y
+choco upgrade chocolateygui -y
+
+choco upgrade cascadia-code-nerd-font -y
+choco upgrade firacode -y
+choco upgrade firacodenf -y
+choco upgrade terminal-icons.powershell -y
+
+choco upgrade mingw -y
+choco upgrade ffmpeg -y
+choco upgrade gawk -y
+choco upgrade patch -y
+choco upgrade sed -y
+
+choco upgrade ventoy -y
+choco upgrade waifu2x-caffe -y
+choco upgrade youtube-dl -y
+choco upgrade assistant-computer-control -y
+
+choco upgrade autoclicker -y
+choco upgrade ds4windows -y
+choco upgrade itch -y
+choco upgrade origin -y
