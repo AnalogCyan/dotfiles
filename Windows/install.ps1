e8a61995e94bf9ab5bfb7b6d8a853165bb3c50d4
AnalogCyan <git@thayn.me>
2022-04-30 16:29:44 -0500

    Misc. work on m oving to new installer.

--- Windows/install.ps1
+++ Windows/install.ps1
@@ -12,68 +12,68 @@ function pwshProfile {
   Copy-Item -Force ".\profile.ps1" -Destination (New-Item -Path "$CurrentUserAllHosts_Path\" -ItemType "file" -Name "Profile.ps1" -Force)
   Copy-Item -Force .\functions -Destination $CurrentUserAllHosts_Path\ -Recurse
 }
-
-function wslRestart {
-  Write-Output "powershell.exe -NoProfile $curDir\install.ps1 -w" > "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd"
-  Write-Warning "A reboot is required to finish setup. Press enter to reboot.`nScript will resume after reboot."
-  Restart-Computer -Confirm
-}
+#
+#function wslRestart {
+#  Write-Output "powershell.exe -NoProfile $curDir\install.ps1 -w" > "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd"
+#  Write-Warning "A reboot is required to finish setup. Press enter to reboot.`nScript will resume after reboot."
+#  Restart-Computer -Confirm
+#}
 
 # ! Running wsl-distro installer as admin makes it only accessable to admin consoles
 # ! Run check to ensure running in powershell and not pwsh, as running updates
 # ! in pwsh when pwsh has an update can break it
-if ($d) {
-  if (Get-Command chocolatey.exe -ErrorAction SilentlyContinue) {
-    Write-Output "Existing Chocolatey install detected, attempting updates..."
-    choco upgrade all -y
-  }
-  else {
-    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-  }
-  Write-Output "Installing tools and dependencies..."
-  choco install chocolateygui choco-protocol-support logitech-options firacode-ttf powertoys powershell-core vscode msiafterburner samsung-magician hwinfo hwmonitor cpu-z gpu-z sqlitebrowser wireshark nmap git mingw python androidstudio flutter adb vim sed grep gawk patch diffutils gnupg keybase -y
-  winget install Microsoft.SurfaceDuoEmulator
-  if ($p) {
-    Write-Warning "A new version of PowerShell was installed, profiles & functions must now be updated."
-    pwshProfile
-  }
-  $sw = Read-Host -Prompt 'Install gaming software? (y/N)'
-  if ($sw -eq "y" -or $sw -eq "Y") {
-    choco install ds4windows playnite minecraft steam itch battle.net epicgameslauncher origin uplay autoclicker steam-cleaner borderlessgaming xsplit-gamecaster -y
-  }
-  $sw = Read-Host -Prompt 'Install additional software? (y/N)'
-  if ($sw -eq "y" -or $sw -eq "Y") {
-    choco install firefox 7zip vlc autohotkey malwarebytes adwcleaner filezilla waifu2x-caffe inkscape rufus youtube-dl youtube-dl-gui audacity deluge kdenlive windscribe teracopy discord unchecky screentogif dopamine sdformatter rpi-imager -y
-  }
-  if ([System.Environment]::OSVersion.Version.Build -ge 18917) {
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart | Out-Null
-    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart | Out-Null
-    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart | Out-Null
-    wslRestart
-  }
-  elseif ([System.Environment]::OSVersion.Version.Build -lt 18917) {
-    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart | Out-Null
-    wslRestart
-  }
-  else {
-    Write-Error -Message "Unrecognized system build number."
-    Break
-  }
-}
-
-if ($w) {
-  wsl --set-default-version 2
-  Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu -OutFile Ubuntu.appx -UseBasicParsing
-  Add-AppxPackage .\Ubuntu.appx
-  Write-Warning ""
-  Start-Process wsl.exe
-  Read-Host -Prompt 'Opening wsl for the first time. Once you complete setup, come back here and press enter to continue.'
-  wsl -- sudo apt update
-  wsl -- sudo apt upgrade
-  wsl -- ../Linux/install.sh
-  Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd" -Force
-  Break
-}
+#if ($d) {
+#  if (Get-Command chocolatey.exe -ErrorAction SilentlyContinue) {
+#    Write-Output "Existing Chocolatey install detected, attempting updates..."
+#    choco upgrade all -y
+#  }
+#  else {
+#    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+#  }
+#  Write-Output "Installing tools and dependencies..."
+#  choco install chocolateygui choco-protocol-support logitech-options firacode-ttf powertoys powershell-core vscode msiafterburner samsung-magician #hwinfo hwmonitor cpu-z gpu-z sqlitebrowser wireshark nmap git mingw python androidstudio flutter adb vim sed grep gawk patch diffutils gnupg keybase -y
+#  winget install Microsoft.SurfaceDuoEmulator
+#  if ($p) {
+#    Write-Warning "A new version of PowerShell was installed, profiles & functions must now be updated."
+#    pwshProfile
+#  }
+#  $sw = Read-Host -Prompt 'Install gaming software? (y/N)'
+#  if ($sw -eq "y" -or $sw -eq "Y") {
+#    choco install ds4windows playnite minecraft steam itch battle.net epicgameslauncher origin uplay autoclicker steam-cleaner borderlessgaming #xsplit-gamecaster -y
+#  }
+#  $sw = Read-Host -Prompt 'Install additional software? (y/N)'
+#  if ($sw -eq "y" -or $sw -eq "Y") {
+#    choco install firefox 7zip vlc autohotkey malwarebytes adwcleaner filezilla waifu2x-caffe inkscape rufus youtube-dl youtube-dl-gui audacity deluge #kdenlive windscribe teracopy discord unchecky screentogif dopamine sdformatter rpi-imager -y
+#  }
+#  if ([System.Environment]::OSVersion.Version.Build -ge 18917) {
+#    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart | Out-Null
+#    dism.exe /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all /norestart | Out-Null
+#    dism.exe /online /enable-feature /featurename:VirtualMachinePlatform /all /norestart | Out-Null
+#    wslRestart
+#  }
+#  elseif ([System.Environment]::OSVersion.Version.Build -lt 18917) {
+#    Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart | Out-Null
+#    wslRestart
+#  }
+#  else {
+#    Write-Error -Message "Unrecognized system build number."
+#    Break
+#  }
+#}
+#
+#if ($w) {
+#  wsl --set-default-version 2
+#  Invoke-WebRequest -Uri https://aka.ms/wsl-ubuntu -OutFile Ubuntu.appx -UseBasicParsing
+#  Add-AppxPackage .\Ubuntu.appx
+#  Write-Warning ""
+#  Start-Process wsl.exe
+#  Read-Host -Prompt 'Opening wsl for the first time. Once you complete setup, come back here and press enter to continue.'
+#  wsl -- sudo apt update
+#  wsl -- sudo apt upgrade
+#  wsl -- ../Linux/install.sh
+#  Remove-Item -Path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Startup\startup.cmd" -Force
+#  Break
+#}
 
 $pfunc = Read-Host -Prompt 'Install pwsh profile/functions? (y/N)'
 $ahk = Read-Host -Prompt 'Install ahk scripts? (y/N)'
@@ -107,12 +107,12 @@ if ($ahk -eq "y" -or $ahk -eq "Y") {
 
 if ($wterm -eq "y" -or $wterm -eq "Y") {
   Write-Output "Copying Terminal config into $env:HOMEPATH\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState\..."
-  Copy-Item -Force '.\settings.json' -Destination "$env:HOMEPATH\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
+  Copy-Item -Force '.\Terminal\settings.json' -Destination "$env:HOMEPATH\AppData\Local\Packages\Microsoft.WindowsTerminal_8wekyb3d8bbwe\LocalState"
 }
 
 if ($gitconf -eq "y" -or $gitconf -eq "Y") {
   Write-Output "Copying git config into $env:HOMEPATH\..."
-  Copy-Item -Force '.\.gitconfig' -Destination "$env:HOMEPATH"
+  Copy-Item -Force '..\.gitconfig' -Destination "$env:HOMEPATH"
 }
 
 if ($mcsym -eq "y" -or $mcsym -eq "Y") {
