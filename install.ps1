211f3a45340a1cbbea8b867b2a65edb1ccedfc78
AnalogCyan <git@thayn.me>
2022-10-19 16:38:33 -0500

    Added admin check & fixed bug w/ home path.

--- install.ps1
+++ install.ps1
@@ -1,4 +1,8 @@
 #!/usr/bin/env pwsh
+if ((id -u) -eq 0) {
+    Write-Error "This script should not be run as administrator!"
+    exit 1
+}
 
 function InstallLinux {
     if (-Not $(Get-Command "apt")) {
@@ -8,21 +12,21 @@ function InstallLinux {
 
     sudo bash -c "apt update --fix-missing && apt upgrade && apt autoremove && apt --fix-broken install"
     sudo apt install gcc g++ git vim htop zsh fish fortune mosh screen
-    
+
 }
 
 function InstallMacOS {
-    New-Item -Path "~/bin/apps/pfetch/" -ItemType Directory -Force
-    Copy-Item -Path "./macOS/Users/cyan/bin/" -Destination "~/bin/" -Force -Recurse
-    git clone "https://github.com/dylanaraps/pfetch.git" "~/bin/apps/pfetch/"
+    New-Item -Path "$HOME/bin/apps/pfetch/" -ItemType Directory -Force
+    Copy-Item -Path "./macOS/Users/cyan/bin/" -Destination "$HOME/bin/" -Force -Recurse
+    git clone "https://github.com/dylanaraps/pfetch.git" "$HOME/bin/apps/pfetch/"
 
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
     sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 
-    New-Item -Path "~/.oh-my-zsh/custom/" -ItemType Directory -Force
-    Move-Item -Path "~/.zshrc" -Destination "~/.zshrc.dotbak" -Force
-    Copy-Item -Path "./macOS/Users/cyan/.zshrc" "~/.zshrc" -Force
+    New-Item -Path "$HOME/.oh-my-zsh/custom/" -ItemType Directory -Force
+    Move-Item -Path "$HOME/.zshrc" -Destination "$HOME/.zshrc.dotbak" -Force
+    Copy-Item -Path "./macOS/Users/cyan/.zshrc" "$HOME/.zshrc" -Force
     Copy-Item -Path "./macOS/Users/cyan/.oh-my-zsh/custom/" -Destination "$(zsh -c 'echo ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}')" -Force -Recurse
 
     ln -s "$HOME/Library/Mobile Documents/com~apple~CloudDocs/Minecraft/Install" "$HOME/Library/Application Support/minecraft"
