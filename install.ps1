2aeb4b26426dc8e7f004b66dc47f0e8942b1b1aa
AnalogCyan <git@thayn.me>
2025-03-02 00:07:27 -0600

    Add Microsoft WSL to the list of winget applications for installation

--- install.ps1
+++ install.ps1
@@ -24,6 +24,7 @@ $WINGET_APPS = @(
   "Microsoft.PowerToys"
   "Microsoft.VisualStudioCode"
   "Python.Python3"
+  "Microsoft.WSL"
   "Canonical.Ubuntu"
   "Microsoft.DevHome"
   "Starship.Starship"
